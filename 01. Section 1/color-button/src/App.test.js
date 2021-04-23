6dee03e8db3151f69a4b6173c64b948dc1e6176a
Noelia Donato <ndonatovignais@santandertecnologia.com.ar>
2021-04-23 09:04:16 -0300

    avance del curso hasta clase 19

--- 01. Section 1/color-button/src/App.test.js
+++ 01. Section 1/color-button/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// I - 12. Start Color Button App
+// I - 13. Test that Finds Button by Role and Clicks Button
+// I - 14. OPTIONAL React Code: Click Button to Change Color
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red'});
+
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
+  expect(colorButton.textContent).toBe('Change to red');
 });
+// F - 12. Start Color Button App
+// F - 13. Test that Finds Button by Role and Clicks Button
+// F - 14. OPTIONAL React Code: Click Button to Change Color
+
+
+// I - 16. Test Initial Condition of Button and Checkbox
+test('initial conditions', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', /Change to blue/i);
+  
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  
+  expect(checkbox).not.toBeChecked();
+});
+// F - 16. Test Initial Condition of Button and Checkbox
