c47d93b4d9737b8da83ab244cbff0a66aac89f6e
Noelia Donato <ndonatovignais@santandertecnologia.com.ar>
2021-05-05 23:23:04 -0300

    avances hasta fin de sección 3

--- 01. Section 1/color-button/src/App.js
+++ 01. Section 1/color-button/src/App.js
@@ -1,20 +1,34 @@
 import { useState } from 'react'
 import './App.css';
 
+export function replaceCamelWithSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+}
+
 function App() {
   const [buttonColor, setButtonColor] = useState('red')
+  const [disabled, setDisabled] = useState(false)
 
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
 
   return (
     <div className="App">
       <button
-        style={{ backgroundColor: buttonColor }}
-        onClick={() => setButtonColor(newButtonColor)}>
+        style={{ backgroundColor: buttonColor, color: 'white' }}
+        onClick={() => setButtonColor(newButtonColor)}
+        disabled={disabled}
+      >
         Change to {newButtonColor}
       </button>
 
-      <input type="checkbox"/>
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
--- 01. Section 1/color-button/src/App.test.js
+++ 01. Section 1/color-button/src/App.test.js
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import App from './App';
+import App, { replaceCamelWithSpaces } from './App';
 
 // I - 12. Start Color Button App
 // I - 13. Test that Finds Button by Role and Clicks Button
@@ -32,3 +32,31 @@ test('initial conditions', () => {
   expect(checkbox).not.toBeChecked();
 });
 // F - 16. Test Initial Condition of Button and Checkbox
+
+// I - 20. Finding Checkbox with Label
+// I - 22. Unit Testing Functions
+test('Checkbox disables button on first click and enable on second click', () => {
+  render(<App />);
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button'});
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeDisabled();
+  
+  fireEvent.click(checkbox);
+  expect(colorButton).toBeEnabled();
+});
+// F - 20. Finding Checkbox with Label
+// F - 22. Unit Testing Functions
+
+describe('spaces before camel-case capital letters', () => {
+  test('works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red');
+  })
+  test('works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  })
+  test('works for multiple inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  })
+})
